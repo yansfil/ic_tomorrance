1d7fadc66fd396a3b0e55a1e6250a46c93161681
yansfil <tansfil@gmail.com>
2018-05-25 16:34:09 +0900

    update with SharedPreferenceBase

--- app/src/main/java/com/tomorrance/yonsei/tomo/Network/SharedPreferenceBase.java
+++ app/src/main/java/com/tomorrance/yonsei/tomo/Network/SharedPreferenceBase.java
@@ -16,25 +16,25 @@ import java.util.Set;
 public class SharedPreferenceBase{
     private static SharedPreferences sp;
     public static void putSharedPreference(String key, HashSet<String> value){
-        sp = _Application.getInstance().getSharedPreferences("cookie-storage", Context.MODE_PRIVATE);
+        sp = _Application.getInstance().getSharedPreferences("storage", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putStringSet(key,value);
         editor.commit();
     }
     public static void putSharedPreference(String key, String value){
-        sp = _Application.getInstance().getSharedPreferences("cookie-storage", Context.MODE_PRIVATE);
+        sp = _Application.getInstance().getSharedPreferences("storage", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putString(key,value);
         editor.commit();
     }
 
     public static Set<String> getSharedPreference(String key, @Nullable Set<String> defaultValue){
-        sp = _Application.getInstance().getSharedPreferences("cookie-storage", Context.MODE_PRIVATE);
+        sp = _Application.getInstance().getSharedPreferences("storage", Context.MODE_PRIVATE);
         return sp.getStringSet(key,defaultValue);
     }
 
     public static String getSharedPreference(String key, @Nullable String defaultValue){
-        sp = _Application.getInstance().getSharedPreferences("cookie-storage", Context.MODE_PRIVATE);
+        sp = _Application.getInstance().getSharedPreferences("storage", Context.MODE_PRIVATE);
         return sp.getString(key,defaultValue);
     }
 
